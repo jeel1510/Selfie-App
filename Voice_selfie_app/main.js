e8129f0823fff8acb17dab3ca569852e06832d0f
jeel1510 <92640751+jeel1510@users.noreply.github.com>
2021-12-10 05:06:50 -0800

    Add files via upload

---
+++ Voice_selfie_app/main.js
@@ -0,0 +1,76 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML ="";
+    recognition.start();
+}
+
+recognition.onresult = function(event) {
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    if(Content == "Take my selfie") 
+    {
+        console.log("Taking Selfie....");
+        speak()
+    }
+}
+ 
+function speak()
+{
+    var synth = window.speechSynthesis;
+
+    speak_data = "Taking your selfie in 5 seconds";
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+
+    setTimeout(function()
+
+    {
+        take_snapshot();
+
+        save();
+
+    }, 5000)
+
+}
+
+camera = document.getElementById("camera");
+
+Webcam.set({ 
+    width: 320,
+
+    height: 240,
+
+    image_format: 'jpeg',
+
+    jpeg_quality: 90 
+});
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_url) {
+
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_url+'"/>';
+
+    });
+}
+
+function save()
+{
+    link = document.getElementById("link");
+
+    Image = document.getElementById("selfie_image").src;
+
+    link.href=image;
+
+    link.click();
+
+}
